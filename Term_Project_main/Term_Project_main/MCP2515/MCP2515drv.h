f34790b95f520ccd426b5bca4291e33fac96082a
Hollingen <eirikholling@hotmail.com>
2022-10-06 13:52:53 +0200

    small edit

--- Term_Project_main/Term_Project_main/MCP2515/MCP2515drv.h
+++ Term_Project_main/Term_Project_main/MCP2515/MCP2515drv.h
@@ -160,9 +160,9 @@ Copyright 2003 Kimberly Otten Software Consulting
 
 
 
-uint8_t mcp2515_init();
+uint8_t mcp2515_init(void);
 uint8_t mcp2515_read(uint8_t address);
-void mcp2515_reset();
+void mcp2515_reset(void);
 void mcp2515_write(uint8_t address, uint8_t data);
 
 
