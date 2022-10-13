ce9ce081cf5b3579c97952fed10e10df12140f6b
Hollingen <eirikholling@hotmail.com>
2022-10-13 12:30:21 +0200

    mcp and menu changes

--- Term_Project_main/Term_Project_main/MENU/MENUdrv.c
+++ Term_Project_main/Term_Project_main/MENU/MENUdrv.c
@@ -1,23 +1,6 @@
 #include "MENUdrv.h"
-#include "OLEDdrv.h"
-#include <stdlib.h>
-#include <util/delay.h>
-#include <avr/pgmspace.h>
 
 
-
-
-
-
-const char text_main_menu[] PROGMEM = "Main menu";
-const char text_play[] PROGMEM = "Play :)";
-const char text_settings[] PROGMEM = "Settings";
-const char text_highscores[] PROGMEM = "Highscores";
-const char text_placeholder1[] PROGMEM = "placeholder1";
-const char text_placeholder2[] PROGMEM = "placeholder2";
-const char text_placeholder3[] PROGMEM = "placeholder3";
-const char text_placeholder4[] PROGMEM = "placeholder4";
-const char text_quit[] PROGMEM = "Quit";
 
 //Add strings to use in game. Update PROGMEM according to 
 const char *const menu_main_strings[] PROGMEM = {
@@ -36,7 +19,7 @@ const char *const menu_main_strings[] PROGMEM = {
 //Have to use this to copy the value from memory into the program
 // strcpy_P(buffer, (char *)pgm_read_word(&(menu_main_strings[i])));
 //It will be copied into this buffer
-char buffer[15];  //15 is the max length of chars on the screen to avoid scrolling
+char buffer[16];  //15 is the max length of chars on the screen to avoid scrolling
                   //The screen is really 16 but we use on space on the front for - to indicate where we are
 
 uint8_t menu_main_counter = 0;
@@ -46,9 +29,11 @@ uint8_t menu_children_counter = 0;
 void menu_print_screen(uint8_t menu_main_counter, uint8_t menu_children_counter){
     oled_reset();
     oled_home();
-    oled_print_char("-");
+	oled_goto_pos(4,1);
+    oled_print_char('>');
+	oled_home();
     for (int i = 0; i < 8; i++){
-        oled_goto_pos(i, 8);
+        oled_goto_pos(i, 16);
         strcpy_P(buffer, (char *)pgm_read_word(&(menu_main_strings[i])));
         oled_print(buffer);
     }
