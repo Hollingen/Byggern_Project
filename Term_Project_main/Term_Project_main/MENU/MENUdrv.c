ce4d0ef3fb24405cf977c316d477ebcc18ae6175
WilliamHaugerud <74253011+WilliamHaugerud@users.noreply.github.com>
2022-10-13 12:51:12 +0200

    try to add scrolling logic

--- Term_Project_main/Term_Project_main/MENU/MENUdrv.c
+++ Term_Project_main/Term_Project_main/MENU/MENUdrv.c
@@ -20,6 +20,7 @@ const char text_placeholder4[] PROGMEM = "placeholder4";
 const char text_quit[] PROGMEM = "Quit";
 
 //Add strings to use in game. Update PROGMEM according to 
+//
 const char *const menu_main_strings[] PROGMEM = {
     text_main_menu,
     text_play,
@@ -36,27 +37,39 @@ const char *const menu_main_strings[] PROGMEM = {
 //Have to use this to copy the value from memory into the program
 // strcpy_P(buffer, (char *)pgm_read_word(&(menu_main_strings[i])));
 //It will be copied into this buffer
-char buffer[15];  //15 is the max length of chars on the screen to avoid scrolling
+char buffer[16];  //15 is the max length of chars on the screen to avoid scrolling
                   //The screen is really 16 but we use on space on the front for - to indicate where we are
 
 uint8_t menu_main_counter = 0;
 uint8_t menu_children_counter = 0;
+uint8_t number_of_strings = 9;
 
 
 void menu_print_screen(uint8_t menu_main_counter, uint8_t menu_children_counter){
+    uint8_t scroll_number;
     oled_reset();
+    oled_goto_pos(menu_main_counter, 0);
+    oled_print_char('>');
     oled_home();
-    oled_print_char("-");
-    for (int i = 0; i < 8; i++){
-        oled_goto_pos(i, 8);
-        strcpy_P(buffer, (char *)pgm_read_word(&(menu_main_strings[i])));
+    for (int i = menu_main_counter; i < 8 + menu_main_counter; i++){
+        scroll_number = i;
+        if(scroll_number > 8){
+            scroll_number = scroll_number - 8;
+        }
+        oled_goto_pos(scroll_number, 16);
+        //Check to see if the counter is higher than number of elements
+        menu_main_scroll_logic();
+        strcpy_P(buffer, (char *)pgm_read_word(&(menu_main_strings[scroll_number])));
         oled_print(buffer);
     }
 
 }
 
 
-
+void menu_main_scroll_logic(uint8_t number_of_strings, uint8_t menu_main_counter){
+    if(menu_main_counter > number_of_strings)
+        menu_main_counter = 0;
+}
 
 
 
