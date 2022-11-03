e95c08f6dd26feb2e9bbf61ea6b2378069c906b7
Hollingen <eirikholling@hotmail.com>
2022-11-03 10:35:00 +0100

    ny stryktur

---
+++ NODE_2/NODE_2/main.c
@@ -0,0 +1,21 @@
+/*
+ * NODE_2.c
+ *
+ * Created: 03.11.2022 10:30:16
+ * Author : eirik
+ */ 
+
+
+#include "sam.h"
+
+
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
