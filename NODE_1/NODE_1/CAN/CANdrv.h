7a28b32f390243fbe12cb89f4cebebf8ee553185
Hollingen <eirikholling@hotmail.com>
2022-11-10 00:37:24 +0100

    added functions in header file

--- NODE_1/NODE_1/CAN/CANdrv.h
+++ NODE_1/NODE_1/CAN/CANdrv.h
@@ -14,3 +14,7 @@ typedef struct{
     char data[8];
 } can_msg;
 
+can_msg can_handle_msg(uint16_t id, uint8_t size, char msg[8]);
+void can_send_msg(can_msg* msg, BUFFER buffer);
+can_msg can_recieve_msg(BUFFER buffer);
+void interrupt_handler(void);
