f2e4d1a021b67bb4d22626834db72d75c4d6728a
WilliamHaugerud <74253011+WilliamHaugerud@users.noreply.github.com>
2022-11-10 15:59:01 +0100

    fixed pwm

--- NODE_2/NODE_2/PWM/PWMdrvperiph.h
+++ NODE_2/NODE_2/PWM/PWMdrvperiph.h
@@ -2,13 +2,13 @@
 #define PWMDRVPERIPH_H_
 
 #include "sam.h"
+#include "sam3x8e.h"
 //#include <avr/io.h>
 //#include <stdio.h>
 //#include <avr/interrupt.h> 
 
 
-void PWM_setup(void);
-void PWM_init_periph(void);
-void TC6_Handler(void);
+
+void PWM_init(void);
 
 #endif
