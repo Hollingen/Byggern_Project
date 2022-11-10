ece90c6b2d0f004e5a6a1f850f54306b94935e5b
Hollingen <eirikholling@hotmail.com>
2022-11-10 17:14:08 +0100

    teste node2

--- NODE_2/NODE_2/Debug/makedep.mk
+++ NODE_2/NODE_2/Debug/makedep.mk
@@ -2,11 +2,17 @@
 # Automatically-generated file. Do not edit or delete the file
 ################################################################################
 
+CAN\can_controller.c
+
+CAN\can_interrupt.c
+
 Device_Startup\startup_sam3xa.c
 
 Device_Startup\system_sam3xa.c
 
 main.c
 
-PWM\PWMdrvperiph.c
+UART\printf-stdarg.c
+
+UART\uart.c
 
--- NODE_2/NODE_2/main.c
+++ NODE_2/NODE_2/main.c
@@ -6,8 +6,9 @@
  */ 
 
 
+#include <stdio.h>
 #include "sam.h"
-#include "../CAN/can_interupt.h"
+#include "CAN/can_interrupt.h"
 
 #define can_br 0x290165
 
@@ -16,12 +17,13 @@ int main(void)
     /* Initialize the SAM system */
 	
     SystemInit();
-	PWM_init();
+	//PWM_init();
+	configure_uart();
     uint8_t check = can_init_def_tx_rx_mb(can_br);
     if(check != 0){
-        exit(0);
+		printf("nei\n\r");
     }
-    configure_UART();
+   
 
     /* Replace with your application code */
     while (1) 
