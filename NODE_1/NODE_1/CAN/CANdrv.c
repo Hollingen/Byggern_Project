b57a812c3f49c47c0d647ac2d0ef07953c06908c
Hollingen <eirikholling@hotmail.com>
2022-11-10 15:19:27 +0100

    canmodul i loopback funke

--- NODE_1/NODE_1/CAN/CANdrv.c
+++ NODE_1/NODE_1/CAN/CANdrv.c
@@ -20,7 +20,7 @@ can_msg can_handle_msg(uint16_t id, uint8_t size, char msg_data[8]){
 void can_send_msg(can_msg* msg, BUFFER buffer){
 
     uint8_t idLSB = (msg->id & 0x7) << 5;
-    uint8_t idMSB = (msg->id & 0x7F8) >> 3;
+    uint8_t idMSB = (msg->id & 0x1F) >> 3;
     //uint8_t buffer0stat;
 
     mcp2515_write(MCP_TXB0SIDH + 16*buffer, idMSB);
@@ -31,6 +31,7 @@ void can_send_msg(can_msg* msg, BUFFER buffer){
     // Checking if the MCP is already requesting transmission in buffer 0
 	uint8_t value;
 	mcp2515_read(MCP_TXB0CTRL, &value);
+	//char* data_bytes = msg->data;
     if((value & 0x08) != 0x08){
         for(uint8_t i = 0; i < msg->data_len; i++){
             mcp2515_write(MCP_TXBD0 + 16*buffer + i, msg->data[i]);
@@ -39,7 +40,8 @@ void can_send_msg(can_msg* msg, BUFFER buffer){
         }
         mcp2515_request_to_send(MCP_RTS_TX0 + buffer);
     }
-    
+    //printf("id %d\n\r", msg.id);
+
 
 }
 
@@ -54,8 +56,11 @@ can_msg can_recieve_msg(BUFFER buffer){
 	mcp2515_read(MCP_RXB0SIDH + 16*buffer, &idMSB);
     uint8_t msg_length;
 	mcp2515_read(MCP_RXB0DLC + 16*buffer, &msg_length);
+	msg_length = msg_length & 0b00001111;
     msg.id = ((idLSB & 0b11100000) >> 5) | ((idMSB & 0b00011111) << 3);
     
+	//printf("length %d\n\r", msg_length);
+
     msg.data_len = msg_length;
 	
     for(uint8_t i = 0; i < msg.data_len; i++){
@@ -104,11 +109,11 @@ void interrupt_handler(){
     if((status & stat_buff1_busy) == stat_buff1_busy){
         printf("BUFFER 1 is currently busy \n\r");
     }
-   //mcp2515_bit_modify(MCP_CANINTF, 0xFF, 0x00); 
+   mcp2515_bit_modify(MCP_CANINTF, 0xFF, 0x00); 
 
 }
 
-/*ISR(INT1_vector){
+ISR(INT1_vector){
     interrupt_handler();
 	printf("nei\n\r");
-}*/
+}
