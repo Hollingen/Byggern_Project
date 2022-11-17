31a2d2369e490401959d72aa48ece1a5dc868e48
EspenHaugen <espesha@stud.ntnu.no>
2022-11-17 16:50:38 +0100

    timer

--- NODE_2/NODE_2/TIMER/timerdrv.c
+++ NODE_2/NODE_2/TIMER/timerdrv.c
@@ -1,3 +1,5 @@
+#include "timerdrv.h"
+
 void timer_init(){
     //Enable TC clock TC0
     
@@ -6,7 +8,19 @@ void timer_init(){
     TC0->TC_CHANNEL[0].TC_CCR |= TC_CCR_CLKEN;
     //Rising edge
     TC0->TC_CHANNEL[0].TC_CMR = TC_CMR_CPCTRG;
-    
 
 
+
+}
+
+void delay_us(uint32_t microsec){
+    //Reset counter
+    uint32_t now_time;
+    TC0->TC_CHANNEL[0].TC_CCR = TC_CCR_SWTRG;
+    //Read counter value
+    //0.000001 / (1/84MHz) / 2 = num of microsec per count
+    uint32_t prev_time = TC0->TC_CHANNEL[0].TC_CV;
+    while(now_time - prev_time < (microsec*(((0.000001)/1/CLK_FREQ)/2))){
+        now_time = TC0->TC_CHANNEL[0].TC_CV;
+    }
 }
