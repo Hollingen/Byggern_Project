06adf3e2f608db8e92b350f6609bf944ef49c53f
WilliamHaugerud <74253011+WilliamHaugerud@users.noreply.github.com>
2022-11-18 15:27:22 +0100

    cleanup

--- NODE_1/NODE_1/SPI/SPIdrv.c
+++ NODE_1/NODE_1/SPI/SPIdrv.c
@@ -6,7 +6,7 @@ void spi_init_master(){
 	DDR_SPI = (1<<DD_MOSI)|(1<<DD_SCK)|(1<<DD_SS);
 		
 	// Enable SPI, Master, set clock rate fck/16
-	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);//|(1<<SPIE);
+	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);
 	//SPCR &= ~((1<<CPOL)|(1<<CPHA));
 	spi_set_ss_high();
 	
